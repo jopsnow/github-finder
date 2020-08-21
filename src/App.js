8b46753dbb401fcff98cf3908df36e7ccfea1c4f
jopsnow <jopsnowball2017@gmail.com>
2020-08-21 18:03:33 +0800

    Updates 08-21-20

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+//layout components
 import Navbar from './components/layouts/Navbar';
 import Alert from './components/layouts/Alert';
-
-import Users from './components/users/Users';
+//users-related components
 import Search from './components/users/Search';
+import Users from './components/users/Users';
+import User from './components/users/User';
+//pages 
+import About from './components/pages/About';
 
 import axios from 'axios';
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
     alert: null
   }
@@ -36,6 +43,24 @@ class App extends Component {
     this.setState({users: res.data.items, loading: false});
   }
 
+  // Get single Github user
+  getUser = async username => {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({user: res.data, loading: false});
+  }
+  // Get user repos
+  getUserRepos = async username => {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({repos: res.data, loading: false});
+  }
   // Clear users from state
   clearUsers = () => this.setState({users: [], loading: false});
 
@@ -47,21 +72,34 @@ class App extends Component {
   }
 
   render() {
-    const { users, loading } = this.state;
+    const { user, users, repos, loading } = this.state;
     return (
-      <div>
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert}/>
-          <Search 
-            searchUsers={this.searchUsers} 
-            clearUsers={this.clearUsers} 
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users}/>
+      <Router>
+        <div>
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert}/>
+            <Switch>
+              <Route exact path="/" render={props => (
+                <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers} 
+                    clearUsers={this.clearUsers} 
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={this.setAlert}
+                  />
+                  <Users loading={loading} users={users}/>
+                </Fragment>
+              )}/>
+              <Route exact path="/about" component={About}/>
+              <Route exact path="/user/:login" render={props => (
+                <User { ...props } getUser={this.getUser} getUserRepos={this.getUserRepos} user={user} repos={repos} loading={loading} />
+              )}/>
+            </Switch>
+            
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
 }
